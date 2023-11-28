09f0437a844d3bd08afda714fc143456212ec6b8
slipperysquid <coleseto123@gmail.com>
2023-11-27 23:08:57 -0800

    test

--- Packet.py
+++ Packet.py
@@ -1,16 +1,47 @@
 import hashlib
 
 class Packet():
-
-    def __init__(self,sender_IP,sender_port, dest_IP,dest_port ,sequence, num_connections,recv_window, data): #all inputs are strings
+    
+    def __init__(self,sender_IP,sender_port, dest_IP,dest_port ,sequence, data): #all inputs are strings
         self.packet = {
-            'senderIP':sender_IP,
+            'senderIP': [int(number).to_bytes(1,byteorder='little',signed=False) for number in sender_IP.split('.') ],
             'senderPort':int(sender_port).to_bytes(2,byteorder='little',signed=False),#16 bit
-            'dest_IP':dest_IP,
+            'dest_IP':[int(number).to_bytes(1,byteorder='little',signed=False) for number in dest_IP.split('.') ],
             'dest_port':int(dest_port).to_bytes(2,byteorder='little',signed=False),#16 bit
-            'sequence': int(sequence).to_bytes(4,byteorder='little',signed=False),#32 bit
-            'checksum': data.encode().digest(),#checksum generated (128 bit)
-            'num_connections':int(num_connections).to_bytes(2,byteorder='little',signed=False),
-            'recv_window':recv_window,
+            'sequence': str(int(sequence).to_bytes(4,byteorder='little',signed=False)),#32 bit
+            'checksum': self.checksum(),#checksum generated (128 bit)
             'data':data.encode()
         }
+        
+    def checksum(self):
+        bytestring = + self.packet['senderPort'] + self.packet['dest_post']+ self.packet['sequence'] + self.packet['data']
+        for num in self.packet['senderIP']:
+            bytestring += num
+        for num in self.packet['dest_IP']:
+            bytestring += num
+
+        return bytestring.digest()
+
+
+    
+
+class Ack():
+    def __init__(self,sender_IP,sender_port, dest_IP,dest_port,sequence,num_connections,recv_window):
+        self.ack = {
+            'senderIP':[int(number).to_bytes(1,byteorder='little',signed=False) for number in sender_IP.split('.') ],
+            'senderPort':int(sender_port).to_bytes(2,byteorder='little',signed=False),#16 bit
+            'dest_IP':[int(number).to_bytes(1,byteorder='little',signed=False) for number in dest_IP.split('.') ],
+            'dest_port':int(dest_port).to_bytes(2,byteorder='little',signed=False),#16 bit
+            'sequence': int(sequence).to_bytes(4,byteorder='little',signed=False),#32 bit
+            'recv_window':int(recv_window).to_bytes(2,byteorder='little',signed=False),
+            'checksum':self.checksum()
+        }
+    
+    def checksum(self):
+        bytestring = + self.packet['senderPort'] + self.packet['dest_post']+ self.packet['sequence'] + self.packet['recv_window']
+        for num in self.packet['senderIP']:
+            bytestring += num
+        for num in self.packet['dest_IP']:
+            bytestring += num
+
+        return bytestring.digest()
