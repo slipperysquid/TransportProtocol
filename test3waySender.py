600d1784998fa60a710ced24130c5eef893a3ebd
slipperysquid <coleseto123@gmail.com>
2023-12-03 00:57:52 -0800

    fixed

--- test3waySender.py
+++ test3waySender.py
@@ -14,8 +14,8 @@ with open("stuff.txt") as f:
 
 print(end - start)
 '''
-proto = Protocol('185.77.96.12',9999)
-conn = proto.conn(dest_IP='127.0.0.1',dest_port=5001)
+proto = Protocol('66.183.48.44',9999)
+conn = proto.conn(dest_IP='185.77.96.12',dest_port=5001)
 
 
 with open("book.txt",encoding="utf8") as f:
