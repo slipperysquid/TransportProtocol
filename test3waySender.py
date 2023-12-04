ba7c3fb4b4eef92d709f3a4af9152f666b525df7
slipperysquid <coleseto123@gmail.com>
2023-12-04 12:27:46 -0800

    more cleaning

--- test3waySender.py
+++ test3waySender.py
@@ -1,9 +1,9 @@
 
 from protocol import Protocol
 #put public ip here or put local ip if testing on local network
-proto = Protocol('66.183.48.44',9999)
+proto = Protocol('',9999)
 #put target ip and port here
-conn = proto.conn(dest_IP='185.77.96.12',dest_port=5001)
+conn = proto.conn(dest_IP='',dest_port=5001)
 
 with open("book.txt",encoding="utf8") as f:
     conn.send_data(f.read().encode())
